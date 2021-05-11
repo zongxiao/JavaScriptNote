ff104b1c0b78d9f3a37f7d3388014b2f2182df29
罗宗校 <905616398@qq.com>
2021-05-11 18:31:12 +0800

    add leetcde

--- leetcode.js
+++ leetcode.js
@@ -314,3 +314,32 @@
 
     console.log( fib(5) ); // 5
 }
+
+
+// 2021-05-11
+// 二叉树
+
+// 树 是一种经常用到的数据结构，用来模拟具有树状结构性质的数据集合
+// 树里的每一个节点有一个值和一个包含所有子节点的列表。从图的观点来看，树也可视为一个拥有N 个节点和N-1 条边的一个有向无环图。
+// 二叉树是一种更为典型的树状结构。如它名字所描述的那样，二叉树是每个节点最多有两个子树的树结构，通常子树被称作“左子树”和“右子树”。
+
+// 树的遍历
+
+    // 前序遍历
+    // 前序遍历首先访问根节点，然后遍历左子树，最后遍历右子树
+    // 地址：https://leetcode-cn.com/leetbook/read/data-structure-binary-tree/xe17x7/
+
+    // 中序遍历
+    // 中序遍历是先遍历左子树，然后访问根节点，然后遍历右子树。
+
+
+    // 后序遍历
+    // 后序遍历是先遍历左子树，然后遍历右子树，最后访问树的根节点。
+
+{
+    var preorderTraversal = function(root) {
+        return root ? [root.val, ...preorderTraversal(root.left), ...preorderTraversal(root.right)] : [];
+    };
+
+    console.log( preorderTraversal( [1,null, 2, 3]) );
+}
