cdf1012d258b5c998494d83d4cfab2d14c4c5c37
罗宗校 <905616398@qq.com>
2021-05-15 18:31:43 +0800

    recall

--- recall.js
+++ recall.js
@@ -3,7 +3,7 @@
 {
     let worker = {
         y: 2,
-        slow (x, z) {
+        slow(x, z) {
             console.log(x * z * this.y);
         },
     }
@@ -13,7 +13,7 @@
     //创建一个函数包装之后延时输出
     function debounce(f, ms) {
         let timeid = null;
-        return function() {
+        return function () {
             clearTimeout(timeid);
             timeid = setTimeout(() => {
                 // f.apply(worker, arguments);
@@ -33,7 +33,7 @@
         'id': '1212121212',
         'parentId': '222'
     };
-    
+
     Object.keys(obj).filter(item => {
         if (item.toLocaleLowerCase().indexOf('id') === -1) return true;
     }).map(item => objnew[item] = obj[item]);
@@ -48,7 +48,7 @@
         from: 1,
         to: 5
     }
-    range[Symbol.iterator] = function() {
+    range[Symbol.iterator] = function () {
         return {
             currentValue: this.from,
             endValue: this.to,
@@ -76,15 +76,15 @@
     function unique(arr) {
         let obj = {};
         let singleArr = arr.filter(item => {
-            let key = typeof(item) + item;
+            let key = typeof (item) + item;
             return obj.hasOwnProperty(key) ? false : (obj[key] = true);
         })
         return singleArr;
     }
-    console.log( unique([1,2,"1","2",1]) );
+    console.log(unique([1, 2, "1", "2", 1]));
 
     // Set特性去重
-    let arr = [1,2,"1","2",1]
+    let arr = [1, 2, "1", "2", 1]
     let newArr = [...new Set(arr)];
     console.log(newArr);
 }
@@ -104,7 +104,7 @@
 
     let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era"];
 
-    console.log( aclean(arr) );
+    console.log(aclean(arr));
 }
 
 {
@@ -123,7 +123,7 @@
         enumerable: true,
         configurable: true
     });
-    
+
     // 浅拷贝
     // let clone = Object.defineProperties({}, Object.getOwnPropertyDescriptors(person));
     let clone = Object.assign({}, person);
@@ -183,7 +183,7 @@
     console.log(visitCountWeakMap.get(userBaby)); // 1
     // userBaby 注销了账号
     userBaby = null;
-    console.log(visitCountWeakMap); 
+    console.log(visitCountWeakMap);
     // WeakMap { <items unknown> }
     // 内存中已经彻底没有了userBaby
 }
@@ -205,7 +205,7 @@
     // new WeakSet()
     let visitWeakSet = new WeakSet();
     let userLuo = { name: 'luo' };
-    let userZhang = { name: 'zhang'};
+    let userZhang = { name: 'zhang' };
     visitWeakSet.add(userLuo);
     visitWeakSet.add(userZhang);
     userLuo = userZhang = null;
@@ -263,7 +263,7 @@
         let reduceSeconds = (end - now) / 1000;
         return reduceSeconds;
     }
-    console.log( getTomorrowSeconds() + 's');
+    console.log(getTomorrowSeconds() + 's');
 
 }
 
@@ -274,7 +274,7 @@
     };
     let meetup = {
         title: 'oc',
-        ocupy: [
+        false: [
             { name: 'jogn' },
             { name: 'hhh' }
         ],
@@ -283,40 +283,168 @@
     // 循环引用
     room.ocupy = meetup;
     meetup.self = meetup;
-    console.log( JSON.stringify(meetup, function (key, value) {
-        console.log(value);
-        if (key != "" && value === meetup) {
+    console.log(JSON.stringify(meetup, function (key, value) {
+        if (value === meetup && key != "") {
             return undefined;
         } else {
             return value;
         }
-    }, 0) );
+    }, 0));
 }
 {
     // 递归遍历对象
+    let staffs = {
+        sale: {
+            salepart1 : [
+                { name: 'zhangsan', salary: 5500 },
+                { name: 'lisi', salary: 7500 }
+            ],
+            salepart2 : [
+                { name: 'wangwu', salary: 12000 },
+                { name: 'zhaoliu', salary: 4500 }
+            ]
+        },
+        development: {
+            javascript: [
+                { name: 'youyuxi', salary: 25000 },
+                { name: 'lzx', salary: 6200 }
+            ],
+            java: [
+                { name: 'lihong', salary: 15000 }
+            ]
+        }
+    }
+
+    function salarySum(staffs) {
+        if (Array.isArray(staffs)) {
+            return staffs.reduce((prev, item) => {
+                return prev + item.salary;
+            }, 0);
+
+        } else {
+            let sum = 0;
+            for (const depart of Object.values(staffs)) {
+                sum += salarySum(depart);
+            }
+            return sum;
+        }
+    }
+
+    console.log( salarySum(staffs) );
 }
 
 {
     // 斐波那契数列
+    // 规律：1 2 3 5 8 13 21...  F(N) = F(N - 1) + F(N - 2);
+    function fib(n) {
+        if (n <= 2) return n;
+        else {
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+    console.log(fib(7));
+
+    function iterationFib(n) {
+        let a = 1;
+        let b = 2;
+        if (n <= 2) return b;
+        for (let i = 3; i <= n; i++) {
+            let c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+    console.log(iterationFib(7));
 }
 {
     // 展开符
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let max = Math.max(...arr);
+    console.log(max);
+
+    let str = "abcd";
+    let arrstr = [...str];
+    console.log(arrstr);
 }
 
 {
     // 作用域闭包
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    function inBetween(start, end) {
+        return item => {
+            return item >= start && item <= end ? true : false;
+        }
+    }
+    console.log(arr.filter(inBetween(3, 6)));
+    function inArray(...arr) {
+        return item => {
+            return arr.includes(item) ? true : false;
+        }
+    }
+    console.log(arr.filter(inArray(1, 3, 4, 10)));
+
 }
 
 {
     // 函数对象属性设置
+    // counter()计数，在counter里面增加set()和decrease()
+    function makeCounter() {
+        let count = 0;
+        let counter = function () {
+            return ++count;
+        }
+        counter.set = function(value) {
+            return count = value;
+        }
+        counter.decrease = function() {
+            return --count;
+        }
+        return counter;
+    }
+    let counter = makeCounter();
+    console.log(counter());
+    console.log(counter());
+    let counter2 = makeCounter();
+    console.log(counter2());
+    console.log(counter2.set(4));
+
 }
 
 {
     // 调度setTimeout
+    let delay = 200000;
+    let timeid = setTimeout(function req() {
+        if (Math.round(Math.random())) {
+            console.log('success...');
+            delay = 200000;
+        } else {
+            console.log('fail...');
+            delay += 200000;
+        }
+        timeid = setTimeout(req, delay);
+    }, delay);
 }
 
 {
     // 缓存包装器
+    let worker = {
+        x: 2,
+        slow (a) {
+            for (let i = 0; i < 1000000000; i++) {
+                a = a + this.x;
+            }
+            return a;
+        }
+    }
+    console.log( worker.slow(4) );
+    console.log( worker.slow(4) );
+
+    function wrapper (func) {
+        let cache = new Map();
+    }
+
+
 }
 
 {
